b6508700c4a658005e5d4b15c917692c6d5f85ab
apaleblueman <gurchetan.singh2@icloud.com>
2024-04-24 17:52:49 +0530

    Add Working JS calculator

--- script.js
+++ script.js
@@ -1,69 +1,115 @@
 //global variables
-let firstNumber, secondNumber;
-let operator;
-const operations = ["add", "substract", "multiply", "divide","equate","clear"]
-const display = document.querySelector(".display");
-const keys = document.querySelector(".keypad");
-//main
-getInput()
-//functions acting in display
-function getInput(){
-    keys.addEventListener('click', (event)=>{
-        // display.textContent = "";
-        let target = event.target;
-        if(!operations.includes(target.id)){
-            display.textContent += target.textContent;
-            
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
+
+let display = document.querySelector(".output");
+
+main();
+
+
+//main function
+function main(){
+    let numbers = document.querySelector(".numpad"); 
+    numbers.addEventListener('click', (event)=>{
+        let target = event.target;        
+        populateDisplay(display, target.textContent);
+    })
+
+    let operators = document.querySelector(".operators");
+    //if opereator is clicked save display value in first number and 
+    //save clicked operator in operator variable
+    operators.addEventListener('click', (e)=>{
+        let target = e.target;
+    
+        if(!operator){
+
+            firstNumber = display.textContent;
+            display.textContent = ''
+            operator = target.id;
+
         }
-        else if(operations.includes(target.id)){
-        if(/*target.id === "equate"*/ firstNumber!== undefined){
+        else if(operator){
+                
                 secondNumber = display.textContent;
-                display.textContent = "";
-                display.textContent = operate(firstNumber,"add",secondNumber);
-            }
-            else{
-                firstNumber = display.textContent;
-                display.textContent = "";
-            }
+                display.textContent = ''
+                firstNumber = operate(firstNumber, operator, secondNumber);
+                secondNumber = '';
+                populateDisplay(display, firstNumber)
+                operator = target.id
+        }
+    })
+
+    let results = document.querySelector(".end-operations");
+    results.addEventListener('click', (e)=>{
+        let target = e.target;
+        if(target.id === "equal"){
+            secondNumber = display.textContent;
+            display.textContent = ''
+            firstNumber = operate(firstNumber, operator, secondNumber);
+            secondNumber = '';
+            populateDisplay(display, firstNumber)
+            operator = ''
+        }
+        else if(target.id === "clear"){
+            firstNumber = '';
+            secondNumber = '';
+            operator = '';
+            display.textContent = '';
+            console.log("clear everyoone")
         }
     })
 }
 //functions
 
-function add(num1, num2){
-    return num1 + num2
+
+//populate display
+function populateDisplay(display, content){
+    if (display.textContent === String(firstNumber)) {
+        // If the display shows the result of the previous operation,
+        // clear it before appending the new number
+        display.textContent = content;
+    } else {
+        // Otherwise, append the number to the display
+        display.textContent += content;
+    }
 }
-function substract(num1, num2){
-    return num1 - num2
+
+//operate
+function operate(firstNumber, operator, secondNumber){
+    switch(operator){
+        case 'add':
+            return add(firstNumber, secondNumber)
+        case 'substract':
+            return substract(firstNumber, secondNumber)
+        case 'multiply':
+            return multiply(firstNumber, secondNumber)
+        case 'divide':
+            return divide(firstNumber, secondNumber)
+    }
 }
-function multiply(num1, num2){
-    return num1 * num2
+
+
+
+//calculations
+
+function add(firstNum, secondNum){
+    return Number(firstNum) + Number(secondNum);
 }
-function divide(num1, num2){
-    if(num2!==0){
-        return num1 / num2
+
+function substract(firstNum, secondNum){
+    return Number(firstNum) - Number(secondNum);
+}
+
+function multiply(firstNum, secondNum){
+    return Number(firstNum) * Number(secondNum);
+}
+
+function divide(firstNum, secondNum){
+    if(secondNum!=0){
+        return Number(firstNum) / Number(secondNum);
     }
     else{
-        return "cannot divide by zero!"
-    }
-}
-//a function to operate on two numbers given a operator
-function operate(num1,operator,num2){
-    switch(operator){
-        case "add":
-            return add(num1, num2);
-            
-        case "substract":
-            return substract(num1, num2);
-            
-        case "multiply":
-            return multiply(num1, num2);
-            
-        case "divide":
-            return divide(num1, num2);
-            
-        default:
-            return "invalid operator"
-            
+        return "Infinity";
     }
 }
